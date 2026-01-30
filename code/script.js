ba033966b838a7834927d6e2fab2225f38fffd5a
Antônio Rocha <antoniojunior159@gmail.com>
2026-01-30 00:16:01 -0300

    Refactor monthlySalary calculation with constants

--- code/script.js
+++ code/script.js
@@ -3,8 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
   // CONFIGURAÇÕES
   // =============================
 
+
+  const safraSalary = 5450;
+  const itauSalary = 5148;
+  const pepsiSalary = 20000;
+  const genialSalary = 7000;
+  const motzSalary = 8000;
+  const groupSc = 8000;
+  const as2Group = 5000;
+  
   // Salário mensal
-  const monthlySalary = 40000;
+  const monthlySalary = safraSalary + itauSalary + pepsiSalary + genialSalary + motzSalary + groupSc + as2Group;
 
   // Dias úteis por mês (média)
   const workDaysPerMonthConfig = 21;
